32f7791449319e9f3c7429689af503e6e3e18f86
Spencer Michalski <semski13@gmail.com>
2023-07-18 17:11:56 -0600

    try removing the weird constants file foo_symbol_data for tests

--- analysis_printer_test.py
+++ analysis_printer_test.py
@@ -3,7 +3,28 @@ from io import StringIO
 from unittest.mock import Mock, patch
 
 from analysis_printer import AnalysisPrinter
-from foo_symbol_data import FOO_SYMBOL_DATA
+from symbol_data import SymbolData
+
+SYMBOL = "FOO"
+OVERVIEW = {"MarketCapitalization": 30000000001}
+BALANCE_SHEET = {
+    "quarterlyReports": [{"totalCurrentAssets": 2000, "totalCurrentLiabilities": 1000}]
+}
+EARNINGS = {
+    "annualEarnings": [{"reportedEPS": 1.33}] * 3 + [{"reportedEPS": 1.0}] * 7,
+    "quarterlyEarnings": [{"reportedEPS": 1.0}] * 12,
+}
+QUOTE = {
+    "Global Quote": {"05. price": "60.00"},
+}
+FOO_SYMBOL_DATA = SymbolData(
+    SYMBOL,
+    OVERVIEW,
+    BALANCE_SHEET,
+    EARNINGS,
+    QUOTE,
+    time_series_monthly_adjusted=None,
+)
 
 
 class TestAnalyze(unittest.TestCase):
