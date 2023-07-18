0e10430b9c595389cf0f811b7bb0e977398dbbae
Spencer Michalski <semski13@gmail.com>
2023-07-18 16:39:52 -0600

    add tests for analysis_printer recommendation

--- analysis_printer_test.py
+++ analysis_printer_test.py
@@ -1,6 +1,38 @@
 import unittest
+from io import StringIO
+from unittest.mock import Mock, patch
+
+from analysis_printer import AnalysisPrinter
+from foo_symbol_data import FOO_SYMBOL_DATA
 
 
 class TestAnalyze(unittest.TestCase):
-    def test_implement():
-        raise NotImplementedError
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_print_buy_recommendation(self, mock_stdout):
+        printer = AnalysisPrinter(FOO_SYMBOL_DATA)
+        printer.recommender.recommendation = Mock(return_value=True)
+        expected_message = """====== RECOMMENDATION ======
+The recommendation for FOO is True
+"""
+        printer.print_recommendation()
+        self.assertEqual(expected_message, mock_stdout.getvalue())
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_print_sell_recommendation(self, mock_stdout):
+        printer = AnalysisPrinter(FOO_SYMBOL_DATA)
+        printer.recommender.recommendation = Mock(return_value=False)
+        expected_message = """====== RECOMMENDATION ======
+The recommendation for FOO is False
+"""
+        printer.print_recommendation()
+        self.assertEqual(expected_message, mock_stdout.getvalue())
+
+    @patch("sys.stdout", new_callable=StringIO)
+    def test_print_sell_recommendation(self, mock_stdout):
+        printer = AnalysisPrinter(FOO_SYMBOL_DATA)
+        printer.recommender.recommendation = Mock(return_value=False)
+        expected_message = """====== RECOMMENDATION ======
+The recommendation for FOO is False
+"""
+        printer.print_recommendation()
+        self.assertEqual(expected_message, mock_stdout.getvalue())
--- symbol_data_test.py
+++ symbol_data_test.py
@@ -1,41 +1,21 @@
 import unittest
-from symbol_data import SymbolData
-
-
-SYMBOL = "FOO"
-OVERVIEW = {"MarketCapitalization": 30000000001}
-BALANCE_SHEET = {
-    "quarterlyReports": [{"totalCurrentAssets": 2000, "totalCurrentLiabilities": 1000}]
-}
-EARNINGS = {
-    "annualEarnings": [{"reportedEPS": 1.33}] * 3 + [{"reportedEPS": 1.0}] * 7,
-    "quarterlyEarnings": [{"reportedEPS": 1.0}] * 12,
-}
-QUOTE = {
-    "Global Quote": {"05. price": "60.00"},
-}
-SYMBOL_DATA = SymbolData(
-    SYMBOL,
-    OVERVIEW,
-    BALANCE_SHEET,
-    EARNINGS,
-    QUOTE,
-    time_series_monthly_adjusted=None,
-)
+from foo_symbol_data import FOO_SYMBOL_DATA
 
 
 class TestSymbolData(unittest.TestCase):
     def test_market_cap(self):
-        self.assertEqual(30000000001, SYMBOL_DATA.market_cap)
+        self.assertEqual(30000000001, FOO_SYMBOL_DATA.market_cap)
 
     def test_current_ratio(self):
-        self.assertEqual(2.0, SYMBOL_DATA.current_ratio)
+        self.assertEqual(2.0, FOO_SYMBOL_DATA.current_ratio)
 
     def test_decade_of_annual_earnings(self):
-        self.assertEqual([1.33] * 3 + [1.0] * 7, SYMBOL_DATA.decade_of_annual_earnings)
+        self.assertEqual(
+            [1.33] * 3 + [1.0] * 7, FOO_SYMBOL_DATA.decade_of_annual_earnings
+        )
 
     def test_earnings_growth_past_decade(self):
-        self.assertAlmostEqual(0.33, SYMBOL_DATA.earnings_growth_past_decade)
+        self.assertAlmostEqual(0.33, FOO_SYMBOL_DATA.earnings_growth_past_decade)
 
     def test_p_e_ratio(self):
-        self.assertEqual(15.0, SYMBOL_DATA.p_e_ratio)
+        self.assertEqual(15.0, FOO_SYMBOL_DATA.p_e_ratio)
